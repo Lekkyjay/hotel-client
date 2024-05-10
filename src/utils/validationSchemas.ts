4d264c0528bd37d002de8df0735d2897e025ba39
‘Lekky <lekkyjay@yahoo.com>
2024-05-10 22:05:08 +0200

    clean up.

--- src/utils/validationSchemas.ts
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi'
-
-const registerValidationSchema = Joi.object({
-  name: Joi.string().required().alphanum().min(3).max(30).label('Name'),
-  email: Joi.string().email().required().label('Email'),
-  password: Joi.string().required().min(6).label('Password'),
-})
-
-export { registerValidationSchema }
